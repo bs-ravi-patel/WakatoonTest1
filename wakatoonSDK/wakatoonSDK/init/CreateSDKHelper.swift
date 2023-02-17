9842326b1fea33139c6a2e3f21da0de7022277d2
Ravi Patel <ravi.patel@bommbaysoftwares.com>
2023-02-17 13:24:31 +0530

    1.0.1 code change

--- wakatoonSDK/wakatoonSDK/init/CreateSDKHelper.swift
+++ wakatoonSDK/wakatoonSDK/init/CreateSDKHelper.swift
@@ -36,4 +36,29 @@
  -output ./WakatoonSDK.xcframework
  
  
+ 
+ 
+ xcodebuild archive \
+ -scheme wakatoonSDK \
+ -archivePath "archives/MyFramework-iOS.xcarchive" \
+ -destination "generic/platform=iOS" \
+ -sdk iphoneos \
+ SKIP_INSTALL=NO \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+ 
+ xcodebuild archive \
+ -scheme wakatoonSDK \
+ -archivePath "archives/MyFramework-iOS-simulator.xcarchive" \
+ -destination "generic/platform=iOS Simulator" \
+ -sdk iphonesimulator \
+ SKIP_INSTALL=NO \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+ 
+ xcodebuild -create-xcframework \
+ -framework "archives/MyFramework-iOS.xcarchive/Products/Library/Frameworks/wakatoonSDK.framework" \
+ -framework "archives/MyFramework-iOS-simulator.xcarchive/Products/Library/Frameworks/wakatoonSDK.framework" \
+ -output "WakatoonSDK.xcframework"
+ 
+ 
+ 
  */
