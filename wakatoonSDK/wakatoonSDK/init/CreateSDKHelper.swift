9028779a7e65bf589075748a00d9dc0f62e54408
Ravi Patel <ravi.patel@bommbaysoftwares.com>
2023-02-20 12:33:34 +0530

    make code changes

--- wakatoonSDK/wakatoonSDK/init/CreateSDKHelper.swift
+++ wakatoonSDK/wakatoonSDK/init/CreateSDKHelper.swift
@@ -12,6 +12,7 @@
  
  xcodebuild archive \
  -scheme wakatoonSDK \
+ -configuration Release \
  -destination "generic/platform=iOS" \
  -archivePath ../output/wakatoonSDK-iOS \
  SKIP_INSTALL=NO \
@@ -20,6 +21,7 @@
  
  xcodebuild archive \
  -scheme wakatoonSDK \
+ -configuration Release \
  -destination "generic/platform=iOS Simulator" \
  -archivePath ../output/wakatoonSDK-Sim \
  SKIP_INSTALL=NO \
@@ -37,28 +39,24 @@
  
  
  
- 
- xcodebuild archive \
- -scheme wakatoonSDK \
- -archivePath "archives/MyFramework-iOS.xcarchive" \
- -destination "generic/platform=iOS" \
- -sdk iphoneos \
- SKIP_INSTALL=NO \
- BUILD_LIBRARY_FOR_DISTRIBUTION=YES
- 
- xcodebuild archive \
- -scheme wakatoonSDK \
- -archivePath "archives/MyFramework-iOS-simulator.xcarchive" \
- -destination "generic/platform=iOS Simulator" \
- -sdk iphonesimulator \
- SKIP_INSTALL=NO \
- BUILD_LIBRARY_FOR_DISTRIBUTION=YES
- 
- xcodebuild -create-xcframework \
- -framework "archives/MyFramework-iOS.xcarchive/Products/Library/Frameworks/wakatoonSDK.framework" \
- -framework "archives/MyFramework-iOS-simulator.xcarchive/Products/Library/Frameworks/wakatoonSDK.framework" \
- -output "WakatoonSDK.xcframework"
- 
- 
- 
  */
+
+
+//xcodebuild clean build \
+//-project wakatoonSDK.xcodeproj \
+//-scheme wakatoonSDK \
+//-configuration Release \
+//-sdk iphonesimulator \
+//-derivedDataPath derived_data
+
+
+//cp -r derived_data/Build/Products/Release-iphonesimulator/wakatoonSDK.framework build/simulator
+
+//xcodebuild clean build \
+//-project wakatoonSDK.xcodeproj \
+//-scheme wakatoonSDK \
+//-configuration Release \
+//-sdk iphoneos \
+//-derivedDataPath derived_data
+//cp -r derived_data/Build/Products/Release-iphoneos/wakatoonSDK.framework build/devices
+
